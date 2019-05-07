0a4c3b8f8db9a7a04512068eefd5b9a363c2079d
Ming Liu <mliu@macpro.home>
2019-05-07 15:57:00 -0400

    dataset

---
+++ apple-nav/src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route } from "react-router-dom";
+
+class Navigation extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return(
+            <div>Hello</div>
+        )
+    }
+}
+
+export default Navigation;
